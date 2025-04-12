760bd280a15b0e666109e113615a2c1f9e6097db
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2025-04-12 11:51:08 +0200

    Create world_code.js

---
+++ bloxd/changeable_wool/world_code.js
@@ -0,0 +1,27 @@
+let tickk = 0;
+
+function onPlayerChangeBlock(id, x, y, z, from, to, nu, nu2, nu3) {
+    if (to === "White Wool") {
+        let curdata = api.getMoonstoneChestItemSlot("-1", 0)["attributes"]["customAttributes"]["blocks"];
+        console.log(curdata);
+        curdata.push([x, y, z]);
+        api.setMoonstoneChestItemSlot("-1", 0, "Dirt", 1, { "customAttributes": { "blocks": curdata } });
+    }
+}
+
+function tick() {
+    if (tickk > 500) {
+        let curdata = api.getMoonstoneChestItemSlot("-1", 0)["attributes"]["customAttributes"]["blocks"];
+        tickk = 0;
+
+        for (let i of curdata) {
+            if (api.getBlock(i[0], i[1], i[2]) === "White Wool") {
+                api.setBlock(i[0], i[1], i[2], "Black Wool");
+            } else {
+                api.setBlock(i[0], i[1], i[2], "White Wool");
+            }
+        }
+    } else {
+        tickk += 1;
+    }
+}
