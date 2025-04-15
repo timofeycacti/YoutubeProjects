ca62486f6066a18f7fc1e58e8d8e7c671366fc89
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2025-04-15 21:11:40 +0200

    Update world_code.js

--- bloxd/customsticks_1/world_code.js
+++ bloxd/customsticks_1/world_code.js
@@ -13,7 +13,12 @@ api.giveItem(id, "Stick", 1, {customDisplayName: "Samurai",customDescription: "U
 api.giveItem(id, "Stick", 1, {customDisplayName: "Prison Stick",customDescription: "Make your enemy stand still!",customAttributes:{abilityid:3}})
 } else if (stickid=="4"){
 api.giveItem(id, "Stick", 1, {customDisplayName: "Piggy Stick!",customDescription: "Spawn piggies and not only them!",customAttributes:{abilityid:4}})
-}
+} else if (stickid=="5"){
+api.giveItem(id, "Stick", 1, {customDisplayName: "Dodger Stick",customDescription: "Become Tensai.\n The most OP stick yet. Use RMB to dash at a cost of 25 hp.\nEven though it damages you, it is very cool price for ability like dash)",customAttributes:{abilityid:5}})
+} else if (stickid=="6"){
+api.giveItem(id, "Stick", 1, {customDisplayName: "Lucky",customDescription: "Lucky lucky lucky me! \nHas chance 1/25 to instakill your enemy.\nRMB does damage or heals\n but can also damage you.\nCan save in extreme situations",customAttributes:{abilityid:6}})
+} 
+
 }
 
 
@@ -23,27 +28,29 @@ function onPlayerDamagingOtherPlayer(attacker,target,dmg){
 item1=api.getHeldItem(attacker)
 //console.log(item1)
 if (item1.attributes["customAttributes"]["abilityid"]==1){
-api.setVelocity(target,0,25,0)
+api.setVelocity(target,0,5,0)
 
 }else if (item1.attributes["customAttributes"]["abilityid"]==2){
 api.setHealth(target,0)
 api.setHealth(attacker,0)
 }else if (item1.attributes["customAttributes"]["abilityid"]==3){
 
-api.applyEffect(target,"Slowness",5000,{inbuiltLevel:100})
+api.applyEffect(target,"Slowness",1000,{inbuiltLevel:5})
 } else if (item1.attributes["customAttributes"]["abilityid"]==4){
-if (api.getMobIds()!=[]){
-api.despawnMob(api.getMobIds()[0])
-}
 let tpos=api.getPosition(target)
-if (Math.random()<0.800){
+api.despawnMob(api.getMobIds()[0])
+if (Math.random()>0.8){
 api.attemptSpawnMob("Pig",tpos[0],tpos[1],tpos[2])
 }
 else{
 api.attemptSpawnMob("Draugr Zombie",tpos[0],tpos[1],tpos[2])
 }
-
-}
+}else if (item1.attributes["customAttributes"]["abilityid"]==5){
+api.applyEffect(attacker,"Speed",1000,{inbuiltLevel:50})
+}else if (item1.attributes["customAttributes"]["abilityid"]==6){
+if (Math.random()>0.95){
+api.killLifeform(target)
+}}
 }
 
 
@@ -53,9 +60,24 @@ api.sendMessage(id,"Hello! I am happy to see somebody here. If you want to messa
 
 }
 
-
-function onPlayerFinishChargingItem(id,used,name){
-
-console.log(`${api.getEntityName(id)} used ${name}`)
+function onPlayerDamagingMob(id,mid){
+api.despawnMob(mid)
 
 }
+function onPlayerAltAction(id){
+ability=api.getHeldItem(id)["attributes"]["customAttributes"]["abilityid"]
+if (ability==5){
+api.applyEffect(id,"Speed",1000,{inbuiltLevel:10})
+api.applyHealthChange(id,-15)
+api.applyEffect(id,"Damage Reduction",1000,{inbuiltLevel:250})
+} else if (ability==6){
+if (Math.random()>0.45){
+api.applyHealthChange(id,-25)
+}
+else {
+api.applyHealthChange(id,25)
+}
+
+}
+}
+
