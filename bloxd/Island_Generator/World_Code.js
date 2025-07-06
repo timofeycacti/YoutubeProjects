da3e071d101db62664d61c22c4f8f5cff94b7c4c
timofeycacti <149524909+timofeycacti@users.noreply.github.com>
2025-07-06 17:52:24 +0200

    Noise Update
    Now this small non-customisable island looks more like naturally-generated.

--- bloxd/Island_Generator/World_Code.js
+++ bloxd/Island_Generator/World_Code.js
@@ -1,47 +1,41 @@
-height=5
-
 function onPlayerAltAction(id,x,y,z,block){
 item= api.getHeldItem(id)!=null? api.getHeldItem(id).name : ""
-if (item!="Arrow of Aura XP"){
+if (item!="Arrow of Aura XP" || block != "Jungle Grass Block"){
 return
 }
-[x,y,z]=api.getPosition(id)
-y-=3
-api.setBlockRect([x-1,y,z-1],[x+1,y,z+1],"Dirt")
+//[x,y,z]=api.getPosition(id)
+y-=2
+api.setBlockRect([x-1,y,z-1],[x+1,y,z+1],"Jungle Grass Block")
 api.setBlockRect([x-2,y+1,z-2],[x+2,y+1,z+2],"Stone")
 api.setBlockRect([x-2,y+2,z-2],[x+2,y+2,z+2],"Grass Block")
+let flower=["Red Tulip","Dandelion","Forget-me-not","Allium","Air"][parseInt(Math.random()*5)]
 
-for (let counter = 0; counter<2; counter++){
-let point = [parseInt(Math.random()*5)-2,parseInt(Math.random()*5)-2]
-pX=parseInt(point[0]+x)
-pY=parseInt(y+3+height)
-pZ=parseInt(point[1]+z)
+generateNoiseSquare(x-2,y+2,z-2,5,5)
+api.setPosition(id,x,y+10,z)
 
-api.setBlockRect([pX,pY,pZ],[pX,pY-1,pZ],"Stone")
-
-api.setBlock([pX,pY+1,pZ],"Grass Block")
-layers=0
-roots=[[pX,pY,pZ]]
-rstate=[false]
-while (!rstate.every(v => v === true && layers < height)){
-for (let rootn = 0; rootn < roots.length; rootn++) {
-    let root = roots[rootn];
-    
-    if (api.getBlock([root[0],root[1]-1,root[2]])){
-        api.setBlock([root[0]+1,root[1]-1,root[2]],"Grass Block")
-        api.setBlock([root[0],root[1]-1,root[2]+1],"Grass Block")
-        api.setBlock([root[0],root[1]-1,root[2]-1],"Grass Block")
-        api.setBlock([root[0]-1,root[1]-1,root[2]],"Grass Block")
-        roots.push([root[0]+1,root[1]-1,root[2]])
-        roots.push([root[0],root[1]-1,root[2]+1])
-        roots.push([root[0],root[1]-1,root[2]])
-        roots.push([root[0]-1,root[1]-1,root[2]])
-        rstate.push(false, false, false, false)
-		layers+=1
-
-    } else {
-        rstate[rootn]=true
-    }}
 }
+
+
+function generateNoiseSquare(x0, y, z0, width, depth) {
+    let scale = 1; 
+    for (let dx = 0; dx < width; dx++) {
+        for (let dz = 0; dz < depth; dz++) {
+            let worldX = x0 + dx;
+            let worldZ = z0 + dz;
+
+            let noiseVal = fakeNoise(worldX * scale, worldZ * scale); 
+            let height = Math.floor(noiseVal * 5); 
+
+            for (let dy = 0; dy <= height; dy++) {
+                let blockType = dy === height ? "Grass Block" : "Dirt";
+                api.setBlock([worldX, y + dy, worldZ], blockType);
+            }
+        }
+    }
 }
+
+
+
+function fakeNoise(x, y) {
+    return Math.sin(x * 0.3 + y * 0.7) * Math.cos(x * 0.5 - y * 0.2);
 }
